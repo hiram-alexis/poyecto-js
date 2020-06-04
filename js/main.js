28ebb878384401a3d478f0f968e6d2139182c070
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-06-04 03:59:17 -0500

    135. Identificación con LocalStorage
    Tube varios problemas al realizar esta practica por algunos detalles de errores que termine detactando y solucionando. #3

--- js/main.js
+++ js/main.js
@@ -82,4 +82,28 @@ $("#to-green").click(function(){
 			}, 500);
 		return false;
 		});
+
+		//Login falso
+	$("#login form").submit(function(){
+		var form_name = $("#form_name").val();
+
+		localStorage.setItem("form_name", form_name);
+	});
+
+	var form_name = localStorage.getItem("form_name");
+	//$("#about p").html("<br><strong>Bienvenido, " + form_name + "</strong>");
+	
+	if (form_name != null && form_name != "undefined") {
+	var about_parrafo = $("#about p");
+		about_parrafo.html("<br><strong>Bienvenido, "+ form_name + "</strong>");	
+		about_parrafo.append("<a href='#' id='logout'> Cerrar sesión</a> ");
+
+		$("#login").hide(); 
+
+		$("#logout").click(function(){
+			localStorage.clear();
+			location.reload();
+		});
+	}
+	
 });
