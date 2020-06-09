065e421a0288e2265aa07139b3f8651f23f90751
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-06-09 03:40:17 -0500

    137. Reloj con JavaScript
    Creacion de un reloj utilizando JavaScript. #3

--- js/main.js
+++ js/main.js
@@ -108,9 +108,17 @@ $("#to-green").click(function(){
 			location.reload();
 		});
 	}
-	
+
+	// Acordeon
 	if (window.location.href.indexOf('about') > -1) {
 		$("#acordeon").accordion();
 	}
-	
+		// Reloj
+	if (window.location.href.indexOf('reloj') > -1) {
+		setInterval(function(){
+			var reloj = moment().format("hh:mm:ss");
+			$('#reloj').html(reloj);
+		}, 1000);
+
+	}
 });
