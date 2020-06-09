f1ae626ac50dae67c32b54ed7def930c3ffc9077
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-06-09 03:15:44 -0500

    136. Acordeón
    Incorporacion de jquery al proyecto, ademas de un acordeon de temas. #3

--- js/main.js
+++ js/main.js
@@ -1,64 +1,67 @@
 $(document).ready(function(){
 	//Slider
- $('.galeria').bxSlider({
-    mode: 'fade',
-    captions: true,
-    slideWidth: 1200,
-    responsive: true,
-    pager: true,
-    touchEnabled: true
-  });
+	if (window.location.href.indexOf('index') > -1) {
+	 $('.galeria').bxSlider({
+	    mode: 'fade',
+	    captions: true,
+	    slideWidth: 1200,
+	    responsive: true,
+	    pager: true,
+	    touchEnabled: true
+	  });
+	}
  //Opciones del Slider: https://bxslider.com/options/
  //Posts
- var posts = [
-{
-title: 'Prueba de titulo 1',
-//date: moment().format("MMMM Do YYYY"),
-//date: moment().format("MMMM dddd YYYY"),
-//date: moment().format("dddd Do MMMM YYYY"),
-date:'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del "+ moment().format("YYYY"),
-content: 'La programación es el proceso utilizado para idear y ordenar las acciones necesarias para realizar un proyecto, preparar ciertas máquinas o aparatos para que empiecen a funcionar en el momento y en la forma deseados o elaborar programas para su empleo en computadoras.'
-},
+if (window.location.href.indexOf('index') > -1) {
+	 var posts = [
 	{
-	title: 'Prueba de titulo 2',
-	//date: new Date(),
+	title: 'Prueba de titulo 1',
+	//date: moment().format("MMMM Do YYYY"),
+	//date: moment().format("MMMM dddd YYYY"),
+	//date: moment().format("dddd Do MMMM YYYY"),
 	date:'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del "+ moment().format("YYYY"),
 	content: 'La programación es el proceso utilizado para idear y ordenar las acciones necesarias para realizar un proyecto, preparar ciertas máquinas o aparatos para que empiecen a funcionar en el momento y en la forma deseados o elaborar programas para su empleo en computadoras.'
 	},
 		{
-		title: 'Prueba de titulo 3',
-		date:'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del "+ moment().format("YYYY"),		content: 'La programación es el proceso utilizado para idear y ordenar las acciones necesarias para realizar un proyecto, preparar ciertas máquinas o aparatos para que empiecen a funcionar en el momento y en la forma deseados o elaborar programas para su empleo en computadoras.'
+		title: 'Prueba de titulo 2',
+		//date: new Date(),
+		date:'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del "+ moment().format("YYYY"),
+		content: 'La programación es el proceso utilizado para idear y ordenar las acciones necesarias para realizar un proyecto, preparar ciertas máquinas o aparatos para que empiecen a funcionar en el momento y en la forma deseados o elaborar programas para su empleo en computadoras.'
 		},
 			{
-			title: 'Prueba de titulo 4',
-			date:'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del "+ moment().format("YYYY"),			content: 'La programación es el proceso utilizado para idear y ordenar las acciones necesarias para realizar un proyecto, preparar ciertas máquinas o aparatos para que empiecen a funcionar en el momento y en la forma deseados o elaborar programas para su empleo en computadoras.'
+			title: 'Prueba de titulo 3',
+			date:'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del "+ moment().format("YYYY"),		content: 'La programación es el proceso utilizado para idear y ordenar las acciones necesarias para realizar un proyecto, preparar ciertas máquinas o aparatos para que empiecen a funcionar en el momento y en la forma deseados o elaborar programas para su empleo en computadoras.'
 			},
 				{
-				title: 'Prueba de titulo 5',
-				date:'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del "+ moment().format("YYYY"),
-				content: 'La programación es el proceso utilizado para idear y ordenar las acciones necesarias para realizar un proyecto, preparar ciertas máquinas o aparatos para que empiecen a funcionar en el momento y en la forma deseados o elaborar programas para su empleo en computadoras.'
+				title: 'Prueba de titulo 4',
+				date:'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del "+ moment().format("YYYY"),			content: 'La programación es el proceso utilizado para idear y ordenar las acciones necesarias para realizar un proyecto, preparar ciertas máquinas o aparatos para que empiecen a funcionar en el momento y en la forma deseados o elaborar programas para su empleo en computadoras.'
 				},
 					{
-					title: 'Prueba de titulo 6',
+					title: 'Prueba de titulo 5',
 					date:'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del "+ moment().format("YYYY"),
 					content: 'La programación es el proceso utilizado para idear y ordenar las acciones necesarias para realizar un proyecto, preparar ciertas máquinas o aparatos para que empiecen a funcionar en el momento y en la forma deseados o elaborar programas para su empleo en computadoras.'
 					},
- ];
+						{
+						title: 'Prueba de titulo 6',
+						date:'Publicado el dia '+ moment().date()+ " de " + moment().format("MMMM") + " del "+ moment().format("YYYY"),
+						content: 'La programación es el proceso utilizado para idear y ordenar las acciones necesarias para realizar un proyecto, preparar ciertas máquinas o aparatos para que empiecen a funcionar en el momento y en la forma deseados o elaborar programas para su empleo en computadoras.'
+						},
+	 ];
 
-posts.forEach((item, index) =>{
-var post = `
-<article class="post">
-					<h2>${item.title}</h2>
-					<span class="date">${item.date}</span>
-					<p>
-						${item.content}
-					</p>
-					<a href="#" class="button-more">Leer más</a>
-				</article>
-`;
-	$("#posts").append(post);
-});
-
+	posts.forEach((item, index) =>{
+	var post = `
+	<article class="post">
+						<h2>${item.title}</h2>
+						<span class="date">${item.date}</span>
+						<p>
+							${item.content}
+						</p>
+						<a href="#" class="button-more">Leer más</a>
+					</article>
+	`;
+		$("#posts").append(post);
+	});
+}
 	//Selector de tema
 var theme =$("#theme");
 $("#to-green").click(function(){
@@ -104,6 +107,10 @@ $("#to-green").click(function(){
 			localStorage.clear();
 			location.reload();
 		});
+	}
+	
+	if (window.location.href.indexOf('about') > -1) {
+		$("#acordeon").accordion();
 	}
 	
 });
